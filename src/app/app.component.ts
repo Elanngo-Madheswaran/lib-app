b4e46804d6f44caeca87c1a08e1995fe7cec0023
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-06-03 21:49:37 +0530

    made a simple version working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+  book_list: string[] = [
+    "Harry Potter",
+    "Ponniyin Selvan"
+  ];
+  book_name: string = "";
+
+  addBook() {
+    this.book_list.unshift(this.book_name);
+    localStorage.setItem("my_books", JSON.stringify(this.book_list));
+    this.book_name = ""; // Clear the input field
+  }
+
+  ngOnInit(): void {
+    const storedBooks = localStorage.getItem("my_books");
+    if (storedBooks) {
+      this.book_list = JSON.parse(storedBooks);
+    }
+  }
 }
